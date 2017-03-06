7dc65b9ac251dd980067b187358a18930a9fb08f
NIcholas Kneafsey <nicholaskneafsey@gmail.com>
2017-03-05 22:40:08 -0800

    Cleaning up for deploy

--- ios/directionsApp/AppDelegate.m
+++ ios/directionsApp/AppDelegate.m
@@ -19,10 +19,10 @@
   NSURL *jsCodeLocation;
   
   //Prod bundle
-  //jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
   
   // Dev bundle below
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  //jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"directionsApp"
--- src/utility/emojiOneHelper.js
+++ src/utility/emojiOneHelper.js
@@ -3,7 +3,7 @@ export const findEmojiUnicode = (emoji) => {
 
   let emojiObj = obj[emoji];
   if (emojiObj === undefined) {
-    console.log(`${emoji} is fucking up`)
+    console.log(`${emoji} does not have an emojiOne emoji associated with it. Check emoji.json`)
   }
   let  emojiUnicode = emojiObj.unicode;
 
